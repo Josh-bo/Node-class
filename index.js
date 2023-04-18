6404cdcdbcf92966587f30569dea25342adf7b7d
Bamidele joshua olayinka <Olayinka775@gmail.com>
2023-04-18 11:35:03 +0100

    I wan to push

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ let userSchema = {
     firstname:{type:String, required:true},
     lastname:{type:String, required:true},
     email:{type:String, required:true, unique:true},
-    password:{type:String, required:true, unique:true},
+    password:{type:String, required:true,},
 }
 
 let userModel = mongoose.model('users_collection', userSchema)
@@ -68,20 +68,51 @@ app.post("/signUp",(req,res) => {
     let form = new userModel(req.body);
     form.save()
     .then(()=>{
-        console.log("Database ti gbaa wole")
-        res.render("signUp", {message :'Sign up successfully'})
+        console.log("Successfully saved")
+        res.redirect("signIn")
+        // res.render("signUp", {message :'Sign up successfully'})
     })
 
     .catch((err) => {
         console.log(err);
-        res.render("signUp", {message : 'Failed to submit'})
+
+        if(err.code === 11000){
+            console.log(err.code);
+            res.render("signUp", {message : 'Email already exist'})
+        }else{
+            res.render("signUp", {message: "Please fill in all fields"})
+        }
     })
 })
 app.get("/signIn", (req,res) => {
-    res.render("signIn")
+    res.render("signIn", {message: ""})
 })
-app.get("/dash", (req,res) => {
-    res.render("dash", {name:'Ajamu', food:'Spag'})
+app.post("/signIn", (req,res) => {
+    userModel.find({email:req.body.email,password:req.body.password})
+    .then((response) => {
+        console.log(response);
+        if(response.length > 0){
+            res.redirect("dash")
+        }else{
+            res.render('signIn', {message: "Incorrect email or password"})
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+app.get("/dash", (req, res) => {
+    // res.render("dash", {name:'Ajamu', food:'Indomie'})
+    // res.render("dash")
+  
+    userModel.find()
+      .then((response) => {
+        res.render("dash", {response})
+        console.log(response);
+  });
+})
+app.get("/edit", (req,res) => {
+    res.render("edit")
 })
 app.listen("4607", () => {
     console.log("server has started on port 4607");
